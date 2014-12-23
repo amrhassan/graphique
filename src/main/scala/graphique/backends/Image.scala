9cb338aa4ffc4add5ad77667714a509898cced71
Amr Hassan <amr.hassan@gmail.com>
2014-12-23 12:06:21 +0200

    Closes #27

--- src/main/scala/graphique/backends/Image.scala
+++ src/main/scala/graphique/backends/Image.scala
@@ -1,7 +1,7 @@
 package graphique.backends
 
 import graphique.images
-import graphique.images.{ImageValidator, ImageAttributes}
+import graphique.images._
 import org.apache.commons.codec.digest.DigestUtils
 
 /**
@@ -32,7 +32,53 @@ case class Image(tag: String, attributes: images.ImageAttributes) {
   /**
    * A MD5 hexadecimal hash value of the attributes.
    */
-  lazy val hashedAttributes: String = DigestUtils md5Hex attributes.toString.toLowerCase
+  lazy val hashedAttributes: String = {
+
+    /**
+     * The hashed attributes value is made up of the following:
+     *
+     *    if (formatAttribute.isEmpty && sizeAttribute.isEmpty)
+     *      ""
+     *    else
+     *      md5Hex(concat(formatAttribute, sizeAttribute))
+     *
+     * Where:
+     *
+     *    md5Hex() is a function which computes the MD5 checksum as a character string of hexadecimal bytes
+     *      of a character string input.
+     *
+     *    concat() produces the concatenation of its two input character strings as a character string.
+     *
+     *    formatAttribute is:
+     *      - an empty character string when the original format is requested
+     *      - "JPEG(quality)" when the JPEG format is requested, where quality is a real value between
+     *        0 and 1.0 indicating the JPEG output quality
+     *      - "PNG" when the PNG format is requested
+     *    sizeAttribute is:
+     *      - an empty character string when the original size is requested
+     *      - "size-within=WIDTHxHEIGHT" when the requested size needs to fit within the values of WIDTH
+     *        and HEIGHT in pixels.
+     */
+
+    val sizeAttribute: String = attributes.size match {
+      case ImageAttribute.OriginalSize => ""
+      case ImageAttribute.SizeWithin(dimensions) => s"size-within=${dimensions.width}x${dimensions.height}"
+    }
+
+    val formatAttribute: String = attributes.format match {
+      case ImageAttribute.OriginalFormat => ""
+      case ImageAttribute.TranscodedFormat(imageFormat) =>
+        imageFormat match {
+          case PNGFormat => "PNG"
+          case JPEGFormat(quality) => s"JPEG($quality)"
+        }
+    }
+
+    if (formatAttribute.isEmpty && sizeAttribute.isEmpty)
+      ""
+    else
+      DigestUtils md5Hex s"$formatAttribute$sizeAttribute"
+  }
 }
 
 object Image {
